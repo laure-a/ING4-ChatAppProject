48644ded790489f1359c885303637ffebfb3c4b4
laure-a <laure.audouy@gmail.com>
2021-10-18 14:02:10 +0200

    MessageForm architecture done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import './App.css';
+import MessageForm from './components/MessageForm.js';
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 
@@ -63,48 +64,10 @@ const styles = {
       backgroundColor: 'rgba(255,255,255,.2)',
     },
   },
-  form: {
-    borderTop: '2px solid #373B44',
-    padding: '.5rem',
-    display: 'flex',
-  },
-  content: {
-    flex: '1 1 auto',
-    marginRight: '.5rem'
-  },
-  send: {
-    backgroundColor: '#D6DDEC',
-    padding: '.2rem .5rem',
-    border: 'none',
-    ':hover': {
-      backgroundColor: '#2A4B99',
-      cursor: 'pointer',
-      color: '#fff',
-    },
-  },
   
 }
 
-const MessageForm = ({
-  addMessage
-}) => {
-  const onSubmit = (e) => {
-    e.preventDefault()
-    const data = new FormData(e.target)
-    addMessage({
-      content: data.get('content'),
-      author: 'david',
-      creation: Date.now(),
-    })
-    e.target.elements.content.value = ''
-  } 
-  return (
-    <form css={styles.form}  onSubmit={onSubmit}>
-      <input type="input" name="content" css={styles.content} />
-      <input type="submit" value="Send" css={styles.send} />
-    </form>
-  )
-}
+
 
 export default ({
   channel = {
---
+++ front-end/src/components/MessageForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MessageForm.css";
+
+const MessageForm = ({
+    addMessage
+  }) => {
+    const onSubmit = (e) => {
+      e.preventDefault()
+      const data = new FormData(e.target)
+      addMessage({
+        content: data.get('content'),
+        author: 'david',
+        creation: Date.now(),
+      })
+      e.target.elements.content.value = ''
+    } 
+    return (
+      <form className="Form"  onSubmit={onSubmit}>
+        <input type="input" name="content" className="Content" />
+        <input type="submit" value="Send" className="Send" />
+      </form>
+    )
+  }
+
+  export default MessageForm
