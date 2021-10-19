f4e153cd50393e9f7e3308ad971e1dc06abc5bee
thomaspoulain0611 <61588557+thomaspoulain0611@users.noreply.github.com>
2021-10-19 13:55:33 +0200

    New logo +color header

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Header.css";
-import icon from "./icon.png";
+import icon from "./img.png";
 
 const Header = ({}) => {
     return (
         <header className="Header">
             <h1>Hello I am your header <img src={icon} width="70px" height="70px"/></h1> 
+            
 
             
 
