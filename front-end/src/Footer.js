9d8c98d72f06577affbc617c479fe7fe2542ffca
laure-a <laure.audouy@gmail.com>
2021-12-14 12:22:12 +0100

    style: header + footer
    css + html for styling header and footer: add info and logos + logout button

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -3,16 +3,26 @@
 
 const styles = {
   footer: {
-    height: '30px',
-    backgroundColor: 'rgba(255,255,255,.3)',
-    flexShrink: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    height: '40px',
+    backgroundColor: '#DDDBDB',
+    fontSize: "14px",
+    color: "black",
+    display: "flex"
   },
+  logo: {
+    width: 30,
+    height: 30,
+    padding: 7
+  }
 }
 
 export default function Footer() {
   return (
     <footer style={styles.footer}>
-      footer
+      <img css={styles.logo} src="/logo_color.png" alt="LogoColor"/>
+      <f4> Made by Laure Audouy and Thomas Poulain, webtech, 2021 </f4>
     </footer>
   );
 }
--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -7,6 +7,7 @@ import { IconButton, Link } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Context from './Context';
 import Gravatar from 'react-gravatar'
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const useStyles = (theme) => ({
   header: {
@@ -15,7 +16,8 @@ const useStyles = (theme) => ({
     alignItems: "center",
     padding: theme.spacing(1),
     backgroundColor: '#DDDBDB',
-    flexShrink: 0
+    flexShrink: 0,
+    minWidth: "600px"
   },
   headerLogIn: {
     backgroundColor: 'red',
@@ -24,13 +26,14 @@ const useStyles = (theme) => ({
     backgroundColor: 'blue',
   },
   menu: {
-    backgroundColor: "blue",
+    color: "black",
+    //backgroundColor: "blue",
     [theme.breakpoints.up('sm')]: {
       display: 'none !important',
     },
   },
   logo: {
-    marginLeft: "6vw",
+    marginRight: "2vw",
     alignItems: "center",
     fontFamily: "Courier",
     //backgroundColor: "red",
@@ -38,14 +41,23 @@ const useStyles = (theme) => ({
     flexGrow: "1",
     display: "block"
   },
+  logoIcon: {
+    marginLeft: "2vw",
+    width: 80,
+    height: 80
+  },
   user: {
     display: "inline-flex",
     alignItems: "center",
     //backgroundColor: "green",
-    marginRight: "6vw",
+    marginRight: "1vw",
     fontSize: 18,
     fontWeight: 500,
     color: "#730202"
+  },
+  logout: {
+    color: 'black',
+
   }
 })
 
@@ -74,27 +86,33 @@ export default function Header({
     >
     <MenuIcon />
     </IconButton>
-      <h1 css={styles.logo}> BeeTalky </h1>
-      {
+    <img css={styles.logoIcon} src="/logo_clear.png" alt="LogoClear"/>
+    <h1 css={styles.logo}> BeeTalky </h1>
+    {
       oauth ?
       <span css={styles.user}>
-        <div>
-          {oauth.email}
-        </div>
-        <Gravatar //gravatar image with default parameters override
-          email={oauth.email}
-          size={50}
-          rating="pg"
-          default="monsterid"
-          className="CustomAvatar-image"
-          style={{margin: '10px', borderRadius: '25px'}}
-          protocol="https://"
-        />
-        <Link onClick={onClickLogout}>logout</Link>
+      <div>
+      {oauth.email}
+      </div>
+      <Gravatar //gravatar image with default parameters override
+      email={oauth.email}
+      size={50}
+      rating="pg"
+      default="monsterid"
+      className="CustomAvatar-image"
+      style={{margin: '10px', borderRadius: '25px'}}
+      protocol="https://"
+      />
+      <IconButton onClick={onClickLogout}
+      size="large"
+      css={styles.logout}
+      sx={{"&:hover": {background: "#cccccc"}}}>
+      <LogoutIcon fontSize="inherit"/>
+      </IconButton>
       </span>
       :
-      <span css={styles.user}> new user</span> 
-      }
+      <span css={styles.user}> new user</span>
+    }
     </header>
   );
 }
