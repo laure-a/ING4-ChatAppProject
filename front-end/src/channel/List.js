324b7737172d9cefa0a742e405b1d9a912cd869b
thomaspoulain0611 <61588557+thomaspoulain0611@users.noreply.github.com>
2021-12-17 01:06:20 +0100

    feat: link between update button and dialog

--- front-end/src/channel/List.js
+++ front-end/src/channel/List.js
@@ -1,11 +1,11 @@
 
 /** @jsxImportSource @emotion/react */
 
-import {forwardRef, useImperativeHandle, useLayoutEffect, useRef, useEffect, useContext, useState} from 'react'
+import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useEffect, useContext, useState } from 'react'
 import axios from 'axios';
 // Layout
 import { useTheme } from '@mui/styles';
-import {Button, IconButton} from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UpdateIcon from '@mui/icons-material/Update';
 import TextField from '@mui/material/TextField';
@@ -15,7 +15,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-import {red, blue} from '@mui/material/colors';
+import { red, blue } from '@mui/material/colors';
 // Markdown
 import { unified } from 'unified'
 import markdown from 'remark-parse'
@@ -88,23 +88,25 @@ export default forwardRef(({
   const { oauth } = useContext(Context);
   const [openDelete, setOpenDelete] = useState(false);
   const [openUpdate, setOpenUpdate] = useState(false);
+  const [newMessage, setNewMessage] = useState('')
   // Expose the `scroll` action
   useImperativeHandle(ref, () => ({
     scroll: scroll
   }));
 
-  const onSubmit = async (channelId,creation) => {
-     await axios.delete(
+  const onSubmit = async (channelId, creation) => {
+    await axios.delete(
       `http://localhost:3001/channels/${channel.id}/messages`
-    , {
-      params: {
-        channelId: `${channelId}`,
-        messageCreation: `${creation}`,
+      , {
+        params: {
+          channelId: `${channelId}`,
+          messageCreation: `${creation}`,
+        }
+      }, {
+      headers: {
+        'Authorization': `Bearer ${oauth.access_token}`
       }
-    },{
-    headers: {
-      'Authorization': `Bearer ${oauth.access_token}`
-    }})
+    })
     deleteMessage(creation)
     handleCloseDelete()
 
@@ -133,7 +135,7 @@ export default forwardRef(({
     setOpenUpdate(false);
   };
 
-  
+
   const rootEl = useRef(null)
   const scrollEl = useRef(null)
   const scroll = () => {
@@ -141,13 +143,13 @@ export default forwardRef(({
   }
   // See https://dev.to/n8tb1t/tracking-scroll-position-with-react-hooks-3bbj
   const throttleTimeout = useRef(null) // react-hooks/exhaustive-deps
-  useLayoutEffect( () => {
+  useLayoutEffect(() => {
     const rootNode = rootEl.current // react-hooks/exhaustive-deps
     const handleScroll = () => {
       if (throttleTimeout.current === null) {
         throttleTimeout.current = setTimeout(() => {
           throttleTimeout.current = null
-          const {scrollTop, offsetHeight, scrollHeight} = rootNode // react-hooks/exhaustive-deps
+          const { scrollTop, offsetHeight, scrollHeight } = rootNode // react-hooks/exhaustive-deps
           onScrollDown(scrollTop + offsetHeight < scrollHeight)
         }, 200)
       }
@@ -160,72 +162,88 @@ export default forwardRef(({
   return (
     <div css={styles.root} ref={rootEl}>
       <div css={styles.nameDiv}>
-      <h1>Messages for {channel.name}</h1>
-      <IconButton
-      css={{marginLeft: 15}}
-      onClick={handleClickOpen}>
-      <PersonAddAltRoundedIcon/>
-      </IconButton>
-      <Dialog open={open} onClose={handleClose}>
-       <DialogTitle>Invite a user</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Add an existing user to this channel by searching its email
-        </DialogContentText>
-        <Autocomplete
-            disablePortal
-            id="combo-box-users"
-            options={[{label: "hey"}, {label: "hey"}]}
-            sx={{ padding: 2, width: 300 }}
-            renderInput={(params) => <TextField {...params} label="User email" />}
-          />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Add</Button>
-      </DialogActions>
-    </Dialog>
+        <h1>Messages for {channel.name}</h1>
+        <IconButton
+          css={{ marginLeft: 15 }}
+          onClick={handleClickOpen}>
+          <PersonAddAltRoundedIcon />
+        </IconButton>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Invite a user</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Add an existing user to this channel by searching its email
+            </DialogContentText>
+            <Autocomplete
+              disablePortal
+              id="combo-box-users"
+              options={[{ label: "hey" }, { label: "hey" }]}
+              sx={{ padding: 2, width: 300 }}
+              renderInput={(params) => <TextField {...params} label="User email" />}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Add</Button>
+          </DialogActions>
+        </Dialog>
       </div>
       <ul>
-        { messages.map( (message, i) => {
-            const {value} = unified()
+        {messages.map((message, i) => {
+          const { value } = unified()
             .use(markdown)
             .use(remark2rehype)
             .use(html)
             .processSync(message.content);
-            return (
-              <li key={i} css={styles.message}>
-                <p>
-                  <span>{message.author}</span>
-                  {' - '}
-                  <span>{dayjs().calendar(message.creation)}</span>
-                  {'     '}
-                  {message.author === oauth.email && (
-                    <span>
-                      <IconButton 
-                       onClick={handleClickOpenDelete}
-                       css={styles.deleteButton}>
-                      <DeleteIcon/>
-                    </IconButton> 
+          return (
+            <li key={i} css={styles.message}>
+              <p>
+                <span>{message.author}</span>
+                {' - '}
+                <span>{dayjs().calendar(message.creation)}</span>
+                {'     '}
+                {message.author === oauth.email && (
+                  <span>
                     <IconButton
-                    onClick={handleClickOpenUpdate}
-                    css={styles.updateButton}>
-                    <UpdateIcon/> 
+                      onClick={handleClickOpenDelete}
+                      css={styles.deleteButton}>
+                      <DeleteIcon />
                     </IconButton>
-                   </span>
-                  )}
-                          <Dialog open={openDelete} onClose={handleCloseDelete}>
-       <DialogTitle>Are you sure that you want to delete this message ?</DialogTitle>
-       <DialogActions>
-           <Button onClick={handleCloseDelete}>Cancel</Button>
-           <Button onClick={()=>onSubmit(channel.id, message.creation)}>Yes</Button>
-       </DialogActions>
-         </Dialog>
-                </p>
-                <div dangerouslySetInnerHTML={{__html: value}}>
-                </div>
-              </li>
-            )
+                    <IconButton
+                      onClick={handleClickOpenUpdate}
+                      css={styles.updateButton}>
+                      <UpdateIcon />
+                    </IconButton>
+                  </span>
+                )}
+                <Dialog open={openDelete} onClose={handleCloseDelete}>
+                  <DialogTitle>Are you sure that you want to delete this message ?</DialogTitle>
+                  <DialogActions>
+                    <Button onClick={handleCloseDelete}>Cancel</Button>
+                    <Button onClick={() => onSubmit(channel.id, message.creation)}>Yes</Button>
+                  </DialogActions>
+                </Dialog>
+                <Dialog open={openUpdate} onClose={handleCloseUpdate}>
+                  <DialogTitle> Enter your new message</DialogTitle>
+                  <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="newMessage"
+              label="New message:"
+              type="text"
+              fullWidth
+              value={newMessage}
+              // onChange={(e) => setNewMessage(e.target.value)}
+              required
+            />
+          </DialogContent>
+                </Dialog>
+              </p>
+              <div dangerouslySetInnerHTML={{ __html: value }}>
+              </div>
+            </li>
+          )
         })}
       </ul>
       <div ref={scrollEl} />
