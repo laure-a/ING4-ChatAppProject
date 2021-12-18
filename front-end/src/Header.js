fde3107fe0321846280f4f6d7c0210f52f48ec76
thomaspoulain0611 <61588557+thomaspoulain0611@users.noreply.github.com>
2021-12-19 00:59:04 +0100

    feat: add avatar selection MUI

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -9,7 +9,12 @@ import Context from './Context';
 import Gravatar from 'react-gravatar'
 import LogoutIcon from '@mui/icons-material/Logout';
 import { red, grey } from '@mui/material/colors';
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import Avatar from '@mui/material/Avatar';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 const useStyles = (theme) => ({
   header: {
     height: 75,
@@ -78,50 +83,109 @@ export default function Header({
     e.stopPropagation()
     setOauth(null)
   }
+
+  const handleCloseSelection = () => {
+    setOpenSelection(false);
+  };
   const [openSelection, setOpenSelection] = useState(false);
   return (
     <header css={styles.header}>
-    <IconButton
-    color="inherit"
-    aria-label="open drawer"
-    onClick={drawerToggle}
-    css={styles.menu}
-    >
-    <MenuIcon />
-    </IconButton>
-    <img css={styles.logoIcon} src="/logo_clear.png" alt="LogoClear"/>
-    <h1 css={styles.logo}> BeeTalky </h1>
-    {
-      oauth ?
-      <span css={styles.user}>
-      <div>
-      {oauth.email}
-      </div>
-      <Gravatar //gravatar image with default parameters override
-      email={oauth.email}
-      size={50}
-      rating="pg"
-      default="monsterid"
-      className="CustomAvatar-image"
-      style={{margin: '10px', borderRadius: '25px'}}
-      protocol="https://"
-      />
-     <Button
-                      onClick={()=>{
-                        setOpenSelection(true);
-                      }}  >
-                        Choose Avatar
-                    </Button>
-      <IconButton onClick={onClickLogout}
-      size="large"
-      css={styles.logout}
-      sx={{"&:hover": {background: grey[200]}}}>
-      <LogoutIcon fontSize="inherit"/>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={drawerToggle}
+        css={styles.menu}
+      >
+        <MenuIcon />
       </IconButton>
-      </span>
-      :
-      <span css={styles.user}> new user</span>
-    }
+      <img css={styles.logoIcon} src="/logo_clear.png" alt="LogoClear" />
+      <h1 css={styles.logo}> BeeTalky </h1>
+      {
+        oauth ?
+          <span css={styles.user}>
+            <div>
+              {oauth.email}
+            </div>
+            {/* user.avatchoice === 0 ?(
+            <Gravatar //gravatar image with default parameters override
+              email={oauth.email}
+              size={50}
+              rating="pg"
+              default="monsterid"
+              className="CustomAvatar-image"
+              style={{ margin: '10px', borderRadius: '25px' }}
+              protocol="https://"
+            />
+            ) : user.avatchoice===1 ?(
+              <img src="https://img.icons8.com/officel/40/000000/avatar.png" />
+            ) : user.avatchoice===2 ?(
+              <img src="https://img.icons8.com/external-flat-icons-pause-08/64/000000/external-avatar-farm-and-garden-flat-icons-pause-08.png" />
+            ) : user.avatchoice===3 ?(
+              <img src="https://img.icons8.com/color/48/000000/spyro.png" />
+            ) : user.avatchoice===4 ?(
+              <img src="https://img.icons8.com/external-photo3ideastudio-lineal-color-photo3ideastudio/64/000000/external-ninja-japan-photo3ideastudio-lineal-color-photo3ideastudio.png" />
+            ) */}
+            <Button
+              onClick={() => {
+                setOpenSelection(true);
+              }}  >
+              Choose Avatar
+            </Button>
+            <IconButton onClick={onClickLogout}
+              size="large"
+              css={styles.logout}
+              sx={{ "&:hover": { background: grey[200] } }}>
+              <LogoutIcon fontSize="inherit" />
+            </IconButton>
+          </span>
+          
+          :
+          <span css={styles.user}> new user</span>
+      }
+      <Dialog open={openSelection} onClose={handleCloseSelection} fullWidth={true} maxWidth='md'>
+        <DialogTitle>Choose an avatar among the selection</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleCloseSelection}>Cancel</Button>
+          <Button
+            onClick= {()=>{
+              setOpenSelection(false);
+            }}
+            >Default Gravatar</Button>
+          <Gravatar //gravatar image with default parameters override
+            email={oauth.email}
+            size={50}
+            rating="pg"
+            default="monsterid"
+            className="CustomAvatar-image"
+            style={{ margin: '10px', borderRadius: '25px' }}
+            protocol="https://"
+          />
+          <Button
+            onClick={() => {
+              setOpenSelection(false);
+            }}
+          >Avatar 1</Button>
+          <img src="https://img.icons8.com/officel/40/000000/avatar.png" />
+          <Button
+            onClick={() => {
+              setOpenSelection(false);
+            }}
+          >Avatar 2</Button>
+          <img src="https://img.icons8.com/external-flat-icons-pause-08/64/000000/external-avatar-farm-and-garden-flat-icons-pause-08.png" />
+          <Button
+          onClick= {()=>{
+            setOpenSelection(false);
+          }}
+          >Avatar 3</Button>
+          <img src="https://img.icons8.com/color/48/000000/spyro.png" />
+          <Button
+          onClick= {()=>{
+            setOpenSelection(false);
+          }}
+          >Avatar 4</Button>
+          <img src="https://img.icons8.com/external-photo3ideastudio-lineal-color-photo3ideastudio/64/000000/external-ninja-japan-photo3ideastudio-lineal-color-photo3ideastudio.png" />
+        </DialogActions>
+      </Dialog>
     </header>
   );
 }
